f61dfe6afabe83c83b767318991d6da1d9c4d778
Ethan <ebonsignori@gmail.com>
2019-03-02 15:08:25 -0500

    added html skeleton and js variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+
+//Declaring Variables
+//Array of every lettter in the alphabet for the computer to choose at random
+var computerGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+                     "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+//Number of guesses the player has - starts at 9, goes down by 1 on an unsuccessful guess
+var guesses = 9;
+
+//Number of times the player successfully guesses the computerGuess
+var wins = 0;
+
+//Number of times the player ran out of guesses
+var losses = 0;
+
+
