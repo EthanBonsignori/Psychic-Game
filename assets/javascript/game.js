dbba8c258465eda51c7b1085bbd12e80dfd8cf3b
Ethan <ebonsignori@gmail.com>
2019-03-03 00:14:33 -0500

    added display of stats to html document

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,45 @@
 
-//Declaring Variables
-//Array of every lettter in the alphabet for the computer to choose at random
+// Declaring Variables
+// Array of every lettter in the alphabet for the computer to choose at random
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-              "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+               "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-//Variable to hold the user's guesses so they cannot resuse them
+// Variable to hold the user's guesses so they cannot reuse them
 var used = [];
 
-//Holds the computers random letter once it is assigned
+// Holds the computers random letter once it is assigned
 var computerGuess;
 
-//Number of times the player successfully guesses the letter
+// Number of times the player successfully guesses the letter
 var wins = 0;
 
-//Number of times the player ran out of guesses
+// Number of times the player ran out of guesses
 var losses = 0;
 
-//Number of guesses the player has - starts at 9, goes down by 1 on an unsuccessful guess
+// Number of guesses the player has - starts at 9, goes down by 1 on an unsuccessful guess
 var guesses = 9;
 
-//Functions
-//Picks a random letter from the letters array
+// Hold references to the places in the HTML where info will be displayed
+var directionsText = document.getElementById("directions-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var guessesText = document.getElementById("guesses-text");
+var usedText = document.getElementById("used-text");
+var tryagainText = document.getElementById("tryagain-text");
+
+// Functions
+// Picks a random letter from the alphabet array
 function pickRandom() {
   computerGuess = alphabet[Math.floor(Math.random()*alphabet.length)];
-  //Reset the # of player guesses when a new letter is picked
+  // Reset the # of player guesses when a new letter is picked
   guesses = 9;
-  //Reset the used array so players can pick those letters again
+  // Reset the used array so players can pick those letters again
   used.length = 0;
   console.log("Computer's Letter: " + computerGuess);
   console.log("9 Guesses Remaining")
 }
 
-//Function run once to initialize the game
+//Run above funcntion once to initialize the game
 pickRandom();
 
 // Captures which key the player presses
@@ -40,32 +48,42 @@ document.onkeyup = function(event) {
 // Captures the key press, converts it to lowercase, and saves it to a variable.
   var playerGuess = event.key.toLowerCase();
 
-// Check if the player guess is in the alphabet (symbols and numbers won't use player guesses, only letters)
+// Check if the key press is in the alphabet (symbols and numbers won't use player guesses, only letters)
   if (alphabet.indexOf(playerGuess) >= 0) {
     console.log("Player's Letter: " + playerGuess);
     if (used.includes(playerGuess)) {
-      alert("Already guessed that letter!");
-    //Check if the player guess is not equal to the computer guess AND hasn't been used by the player before
+      tryagainText.textContent = "You've already guessed that letter, try again!";
+    // Check if the letter the player pressed is not equal to the computer guess
       } else if (playerGuess != computerGuess) {
-        //Adds key to the used variable
+        // Adds the letter the player pressed to the used variable so it cannot be counted twice against guesses
         used.push(playerGuess);
         console.log("Letter's guessed: " + used)
-        //player loses 1 guess
+        // Player loses 1 guess
         guesses = guesses -1;
-        console.log("guesses: " + guesses);
+        console.log("Guesses Remaining: " + guesses);
       
-        //players letter is the same as computer's, add 1 to wins and reset the game
+        // Players letter is the same as computer's, add 1 to wins and reset through the pickRandom function
         } else {
           wins++;
           pickRandom();
           console.log("Wins " + wins);
-    }
+        }
+
+        // Hide the directions
+        directionsText.textContent = "";
+
+        // Display the players wins, losses, guesses remaining, and letters already guesses in the current round
+        winsText.textContent = "Wins: " + wins;
+        lossesText.textContent = "Losses: " + losses;
+        guessesText.textContent = "Guesses remaining: " + guesses;
+        usedText.textContent = "Guessed so far: " + used;
   }
-  //Check if the player is out of guesses
+  // Check if the player is out of guesses
   if (guesses <= 0) {
-  //add 1 to losses and reset the game
+  // Add 1 to losses and reset the game
     losses++;
     pickRandom();
+    console.log("Losses " + losses);
   }
 };
 
